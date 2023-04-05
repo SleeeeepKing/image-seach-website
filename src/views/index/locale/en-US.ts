f6dee0422ba78c932ded8353e1ae614e6d09e3f5
Wenlei <luowenlei724@gmail.com>
2023-04-05 22:17:23 +0200

    add translate

--- src/views/index/locale/en-US.ts
+++ src/views/index/locale/en-US.ts
@@ -1,5 +1,6 @@
 export default {
   'menu.index': 'Home',
-
-
+  'index.search.form.input.placeholder': 'Please enter the search content',
+  'index.search.form.search': 'Search',
+  'index.search.form.search.random': 'See how lucky',
 };
